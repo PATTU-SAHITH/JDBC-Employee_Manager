9446cbc93752e387172f6a8878081043c0cdf61b
PATTU-SAHITH <sahithpattu@gmail.com>
2026-08-31 12:56:08 +0530

    Update DatabaseConnection to use environment variables for database credentials

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/EmployeeManager";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Pattu030@";
+    private static final String URL = System.getenv("DB_URL");
+private static final String USER = System.getenv("DB_USER");
+private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
